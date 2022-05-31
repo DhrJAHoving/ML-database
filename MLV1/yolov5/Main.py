9df15c2a3b31822515e892e01c0e02e6da7285a8
DhrJAHoving <50868801+DhrJAHoving@users.noreply.github.com>
2022-05-31 12:20:13 +0200

    Update Main.py

--- MLV1/yolov5/Main.py
+++ MLV1/yolov5/Main.py
@@ -6,13 +6,14 @@ from Led import Turn_off
 led_pin = 7
 Init_GPIO(led_pin)
 
-image_source = '../data/images/val/2022-05-03-11-25-835.jpg'
-# model???????? = ..........
-confidence_treshold = 0.9
+W = 'Database_V3/Best_one/weights/best.pt'           # weights_source    Best one is anders
+I = '../data/images/val/2022-05-03-11-25-835.jpg'    # image_source
+D = '/data/custom_dataset_V3.yaml'                   # data_source
+C = 0.9                                              # confidence_treshold
 
 while True:
     LED = False
-    LED = detect(source=image_source, conf_thres=confidence_treshold)
+    LED = detect(weights=W, source=I, data=D, conf_thres=C)
     print("Led = ", LED)
     
     if LED = True:
