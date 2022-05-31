9f738de97edb03fdfbbfe5c9ab80946291c3951b
DhrJAHoving <50868801+DhrJAHoving@users.noreply.github.com>
2022-05-31 11:49:52 +0200

    Update Main.py

--- MLV1/yolov5/Main.py
+++ MLV1/yolov5/Main.py
@@ -3,14 +3,21 @@ from Led import Init_GPIO
 from Led import Turn_on
 from Led import Turn_off
 
-
-LED = False
 led_pin = 7
 Init_GPIO(led_pin)
+
 image_source = '../data/images/val/2022-05-03-11-25-835.jpg'
+# model???????? = ..........
 confidence_treshold = 0.9
 
-
-# LED = detect(source=image_source, conf_thres=confidence_treshold)
-
-# print("Led = ", LED)
+while True:
+    LED = False
+    LED = detect(source=image_source, conf_thres=confidence_treshold)
+    print("Led = ", LED)
+    
+    if LED = True:
+        Turn_on(led_pin)
+    else:
+        Turn_off(led_pin)
+        
+#einde
