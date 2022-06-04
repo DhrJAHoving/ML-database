da9c40a53b1cab51df997714ab5f78823223628d
DhrJAHoving <50868801+DhrJAHoving@users.noreply.github.com>
2022-06-04 10:38:23 +0200

    edited detect.py

--- MLV1/yolov5/Call_detect2.py
+++ MLV1/yolov5/Call_detect2.py
@@ -1,10 +1,17 @@
 from detect2 import detect
+# import Jetson.GPIO as GPIO
+# import cv2 ?
 
-LED = False
-image_source = '../data/images/val/2022-05-03-11-25-835.jpg'
+weights_path = 'runs/train/CPU_epochs=30_batch-size=24_image-size=640_database-size=600/weights/best.pt'
 confidence_treshold = 0.9
 
-
-LED = detect(source=image_source, conf_thres=confidence_treshold)
-
-print("Led = ", LED)
+while True:
+    LED = False
+    
+    # Maak foto
+    # Sla foto op
+    image_source = '../datasets/custom_dataset_V3/val/images/2022-05-08-10-33-1644.jpg' #Nieuwste afbeelding
+    LED = detect(weights=weights_path, source=image_source, conf_thres=confidence_treshold)
+   
+    # Zet led aan of uit
+    print("Led = ", LED)
