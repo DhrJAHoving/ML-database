9561357a43110fc5f0d58286847092005906dcfb
DhrJAHoving <50868801+DhrJAHoving@users.noreply.github.com>
2022-06-20 10:57:31 +0200

    Summary

--- MLV1/yolov5/Main.py
+++ MLV1/yolov5/Main.py
@@ -1,17 +1,19 @@
 from detect2 import detect
-from Led import Init_GPIO
-from Led import Turn_on
-from Led import Turn_off
+# from Led import Init_GPIO
+# from Led import Turn_on
+# from Led import Turn_off
 
-led_pin = 7
-Init_GPIO(led_pin)
+# led_pin = 7
+# Init_GPIO(led_pin)
 
-W = 'Database_V3/Best_one/weights/best.pt'           # weights_source    Best one is anders
-I = '../data/images/val/2022-05-03-11-25-835.jpg'    # image_source      Dit zijn de fotos
-D = '/data/custom_dataset_V3.yaml'                   # data_source
-C = 0.9                                              # confidence_treshold
+W = 'Database_V4/SER640x22x200xyolov5s.ptx1/weights/best.pt'           # weights_source    Best one is anders
+I = '../datasets/custom_dataset_V3/val/images/2022-05-08-10-33-819.jpg'    # image_source      Dit zijn de fotos
+D = 'data/custom_dataset_V3.yaml'                   # data_source
+C = 0.85                                              # confidence_treshold
 
-while True:
+i = 0
+# while True:
+while i < 1:
     # Take image
     # Save image on path I
 
@@ -19,7 +21,9 @@ while True:
     LED = detect(weights=W, source=I, data=D, conf_thres=C)
     print("Led = ", LED)
     
-    if LED = True:
-        Turn_on(led_pin)
-    else:
-        Turn_off(led_pin)
+    # if LED == True:
+    #     Turn_on(led_pin)
+    # else:
+    #     Turn_off(led_pin)
+    
+    i = i + 1
